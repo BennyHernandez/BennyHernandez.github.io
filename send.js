7f9f1d3b2be4eb1b6723d28a27497353b8504a7f
BennyFaelz <BennyFaelz@users.noreply.github.com>
2019-10-26 20:27:19 -0700

    Update send.js

--- send.js
+++ send.js
@@ -1,24 +1,31 @@
-$(".enabled-2cQ-u7[aria-label='Mute']").click(function() {
-    sendMute(false);
+$(".enabled-2cQ-u7[aria-label='Mute']").click(function () {
+    sendMute($(this).attr("aria-label"));
 });
 $(".enabled-2cQ-u7[aria-label='Unmute']").click(function () {
-    sendMute(true);
+    sendMute($(this).attr("aria-label"));
 });
 
 function sendMute(value) {
     $.ajax({
         url: 'http://127.0.0.1:5000/lightControl',
         type: 'POST',
-        data: JSON.stringify({ "type": "mute", "value": value}),
+        data: JSON.stringify({ "type": "mute", "value": value }),
         success: function (response) { console.log("Sent to bot!") }
     });
 }
+
+$(".enabled-2cQ-u7[aria-label='Deafen']").click(function () {
+    sendDeafen($(this).attr("aria-label"));
+});
+$(".enabled-2cQ-u7[aria-label='Undeafen']").click(function () {
+    sendDeafen($(this).attr("aria-label"));
+});
 
 function sendDeafen(value) {
     $.ajax({
         url: 'http://127.0.0.1:5000/lightControl',
         type: 'POST',
-        data: JSON.stringify({ "type": "mute", "value": value}),
+        data: JSON.stringify({ "type": "deafen", "value": value }),
         success: function (response) { console.log("Sent to bot!") }
     });
 }
